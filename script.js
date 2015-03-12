a5449878707c36a3953f8fcf74031ab569962aba
volkanozcan <vozcan86@gmail.com>
2015-03-12 12:11:49 +0000

    ikinci

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
+   var a=function rastgele (x,y) {
+	/*x ten y'ye kadar rastgele sayı ver*/
+	return ~~(Math.random() * y) + x;
+}
+ function renkver(){
+     return rastgele(10,200)
+     
+ }
+    setInterval(function(){ 
+        var body = $("body");
+        $(body).css("background-color","red")
+        console.log(a())
+        
+    }, 500);
 
-    
 }); 
-});
